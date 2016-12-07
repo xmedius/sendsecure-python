1bd982231945f783d379d66e473ce63cb24606f1
Silvain Laplante <silvain.laplante@xmedius.com>
2016-12-07 14:23:14 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='sendsecure',
+    version='1.0.0',
+    description='The sendsecure Python module for XMediusSENDSECURE (SendSecure)',
+    long_description='See https://github.com/xmedius/sendsecure-python for more information',
+    url='https://github.com/xmedius/sendsecure-python/',
+    author='XMedius R&D',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    packages=['sendsecure'],
+    install_requires=['pycurl']
+)
+
